3a00596bdc34e8784880eb2bd44309ba13c68b50
Andrew Kang <andrew.b.kang@gmail.com>
2013-08-07 17:53:42 -0400

    actionmailerfix #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 PromiseApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  config.action_mailer.default_url_options = { :host => 'promiseapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://o-ma-gersh-ish-mah-app.herokuapp.com/' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
