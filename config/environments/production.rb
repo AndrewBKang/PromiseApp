6e3cbc49252c79719d5efe47b359c4887fd3edb1
Andrew Kang <andrew.b.kang@gmail.com>
2013-08-18 20:51:46 -0400

    fix #5 host still wrong

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 PromiseApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  config.action_mailer.default_url_options = { :host => 'http://promiseapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'promiseapp.herokuapp.com' }
   
   # ActionMailer::Base.smtp_settings = {
   #   :address        => "smtp.sendgrid.net",
