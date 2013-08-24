bede12ae3842d4ebf3827b2e1fa7eafda494261d
Andrew Kang <andrew.b.kang@gmail.com>
2013-08-23 22:22:56 -0400

    commented out pg migration for future migrations

--- db/migrate/20130824021245_alter_user_id_pgsql.rb
+++ db/migrate/20130824021245_alter_user_id_pgsql.rb
@@ -1,9 +1,9 @@
 class AlterUserIdPgsql < ActiveRecord::Migration
-  def change
-    execute %q{
-      alter table likes
-      alter column user_id
-      type int using cast(user_id as int)
-    }
-  end
+  # def change
+  #   execute %q{
+  #     alter table likes
+  #     alter column user_id
+  #     type int using cast(user_id as int)
+  #   }
+  # end
 end
